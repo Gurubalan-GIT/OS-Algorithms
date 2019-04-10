973561ca235093faf534c4e5840c7d510c350c55
Gurubalan-GIT <gurubalan.work@gmail.com>
2019-04-11 01:22:20 +0530

    Pushed header changes

--- File-Allocation/Sequential/sfa.cpp
+++ File-Allocation/Sequential/sfa.cpp
@@ -1,3 +1,10 @@
+/*
+ * Created Date: Wednesday April 10th 2019
+ * Author: Gurubalan Harikrishnan
+ * Email-ID: gurubalan.work@gmail.com
+ * -----
+ * Copyright (c) 2019 Gurubalan Harikrishnan
+ */
 #include<iostream>
 using namespace std;
 int main(){
--- Scheduling/Priority-Scheduling/SameArrivalTime/ps.cpp
+++ Scheduling/Priority-Scheduling/SameArrivalTime/ps.cpp
@@ -1,3 +1,10 @@
+/*
+ * Created Date: Tuesday April 9th 2019
+ * Author: Gurubalan Harikrishnan
+ * Email-ID: gurubalan.work@gmail.com
+ * -----
+ * Copyright (c) 2019 Gurubalan Harikrishnan
+ */
 #include<iostream>
 using namespace std;
 int main(){
