7237ad00902e0f3cbdeb2af82c5859fb19dd3af9
Gurubalan-GIT <gurubalan.work@gmail.com>
2019-05-06 04:43:50 +0530

    Pushed fix for Paging

---
+++ Page-Replacement/FCFS/fcfs.cpp
@@ -0,0 +1,19 @@
+/*
+ * Created Date: Monday May 6th 2019
+ * Author: Gurubalan Harikrishnan
+ * Email-ID: gurubalan.work@gmail.com
+ * -----
+ * Copyright (c) 2019 Gurubalan Harikrishnan
+ */
+#include<iostream>
+using namespace std;
+int main(){
+    int nf,n,pages[100],frames[100];
+    cout<<"Enter number of pages you want to enter:\n";
+    cin>>n;
+    cout<<"Enter the pages or Page sequence:\n";
+    for(int i=0;i<n;i++){
+        
+    }
+
+}
