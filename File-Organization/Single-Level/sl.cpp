a65311b71869acebc8688ab4e9648bcb9ac458d6
Gurubalan-GIT <gurubalan.work@gmail.com>
2019-05-07 00:00:36 +0530

    Pushed Single level File Organisation changes

---
+++ File-Organization/Single-Level/sl.cpp
@@ -0,0 +1,12 @@
+/*
+ * Created Date: Monday May 6th 2019
+ * Author: Gurubalan Harikrishnan
+ * Email-ID: gurubalan.work@gmail.com
+ * -----
+ * Copyright (c) 2019 Gurubalan Harikrishnan
+ */
+#include<stdio.h>
+using namespace std;
+int main(){
+    int fileno, file[100], 
+}
